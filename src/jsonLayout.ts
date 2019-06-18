b5189c08d0ee4d5d869e60fafe6a2ff9ead98e5f
hehao <hehao@kuaishou.com.cn>
2019-06-18 14:11:33 +0800

    增加fast-safe-stringify

--- src/jsonLayout.ts
+++ src/jsonLayout.ts
@@ -1,6 +1,7 @@
 import {LoggingEvent} from 'log4js';
 import {format} from 'date-fns';
 import {isPlainObject} from 'lodash';
+import stringifySafe from 'fast-safe-stringify';
 
 function formatUTCDate(date: Date) {
     return format(new Date(date), 'YYYY-MM-DD HH:mm:ss.SSS');
@@ -9,32 +10,33 @@ function formatUTCDate(date: Date) {
 // 日志转json
 export function jsonLayout(config: any) {
     return (logEvent: LoggingEvent) => {
-        try {
-            const logMessage = Array.isArray(logEvent.data) ? logEvent.data : [logEvent.data];
-            let message = {};
-            // 如果logMessage是对象, 则展开一层, 否则放在data字段下透传
-            if (isPlainObject(logMessage[0])) {
-                message = {
-                    ...logMessage[0],
-                };
-            } else {
-                message = {
-                    data: logMessage[0],
-                };
-            }
-            const logData = {
-                ...logEvent.context,
-                ...(config.context || {}),
-                date: formatUTCDate(logEvent.startTime),
-                // @ts-ignore
-                level: logEvent.level.levelStr || '',
-                pid: logEvent.pid,
-                time: Date.now(),
-                ...message,
+        const logMessage = Array.isArray(logEvent.data) ? logEvent.data : [logEvent.data];
+        let message = {};
+        // 如果logMessage是对象, 则展开一层, 否则放在data字段下透传
+        if (isPlainObject(logMessage[0])) {
+            message = {
+                ...logMessage[0],
             };
+        } else {
+            message = {
+                data: logMessage[0],
+            };
+        }
+        const logData = {
+            ...logEvent.context,
+            ...(config.context || {}),
+            date: formatUTCDate(logEvent.startTime),
+            // @ts-ignore
+            level: logEvent.level.levelStr || '',
+            pid: logEvent.pid,
+            time: Date.now(),
+            ...message,
+        };
+        try {
             return JSON.stringify(logData);
         } catch (e) {
-            return JSON.stringify({
+            return stringifySafe({
+                ...logData,
                 error: `ERROR CONVERTING LOG MESSAGE TO JSON: ${e.message}`
             });
         }
